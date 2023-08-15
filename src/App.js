55aaf60f035a6ead8283353653f36ffcb774d5d8
philodia <libassedia@hotmail.com>
2023-08-15 17:34:39 +0000

    commti monreact1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Navbar, Nav, Container, Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <>
+      <div className="app">
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="/home">Mon React bootstrap-App 1</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="/home">Accueil</Nav.Link>
+                  <Nav.Link href="/link">Lien</Nav.Link>
+                  <Nav.Link href="/search">recherche</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+  
+          <Container>
+            
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Img variant="top" src="image1.jpg" />
+                  <Card.Body>
+                    <Card.Title>Carte 1</Card.Title>
+                    <Card.Text>Description de la carte 1.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <Card.Img variant="top" src="image2.jpg" />
+                  <Card.Body>
+                    <Card.Title>Carte 2</Card.Title>
+                    <Card.Text>Description de la carte 2.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <Card.Img variant="top" src="image3.jpg" />
+                  <Card.Body>
+                    <Card.Title>Carte 3</Card.Title>
+                    <Card.Text>Description de la carte 3.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
 }
 
 export default App;
